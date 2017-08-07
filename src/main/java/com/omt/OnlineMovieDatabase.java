fd28efabe9697532b031f84fbbacf43e0d2912cf
zoranjankov <zoranjankov99@gmail.com>
2017-08-07 14:28:55 +0200

    Added BaseEntity and basic configuration for SpringBootApp

---
+++ src/main/java/com/omt/OnlineMovieDatabase.java
@@ -0,0 +1,12 @@
+package com.omt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMovieDatabase {
+
+	public static void main(String[] args) {
+        SpringApplication.run(OnlineMovieDatabase.class, args);
+	}
+}
